bce7a2a4a926ee6503d6572b5f34c098d87861fd
Batiste Vancoillie <batiste.vancoillie@student.vives.be>
2025-09-24 11:11:36 +0200

    Initial commit

---
+++ WerkurenCalculator/WerkurenCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WerkurenCalculatorApp.swift
+//  WerkurenCalculator
+//
+//  Created by Batiste Vancoillie on 23/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct WerkurenCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
